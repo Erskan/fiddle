1aa8cdf132ce18b1a68fc1ecb83930d401f9a064
Erik Mattsson Mårn <erikmattssonmarn@gmail.com>
2016-07-17 10:55:52 +0200

    Added running toggle and actually used the render delta.

--- app.js
+++ app.js
@@ -1,56 +1,76 @@
-(function() {
-    'use strict';
+'use strict';
 
-    console.log('Setting initial values...');
-    // Initial declarations
-    var cnv = document.getElementById('cnv');
-    var ctx = cnv.getContext("2d");
-    ctx.translate(cnv.width/2, cnv.height/2);
-    var delta = 0;
-    var lastTimeCalled = 0;
-    var maxFPS = 60;
+console.log('Setting initial values...');
+// Initial declarations
+var isRunning = true;
+var cnv = document.getElementById('cnv');
+var ctx = cnv.getContext("2d");
+ctx.translate(cnv.width/2, cnv.height/2);
+var delta = 0;
+var lastTimeCalled = 0;
+var maxFPS = 60;
+var frameID;
 
-    var testVar = 20;
-    var testSig = 'pos';
+var testVar = 20;
+var testStep = 0.1;
+var testSig = 'pos';
 
-    // The main game loop
-    function mainLoop(timecalled) {
-        if (timecalled < lastTimeCalled + (1000 / maxFPS)) {
-            requestAnimationFrame(mainLoop);
-            return;
-        }
-        delta = timecalled - lastTimeCalled; // get the delta time since last frame
-        lastTimeCalled = timecalled;
-        
-        update(delta);
-        drawCanvas();
-        requestAnimationFrame(mainLoop);
+// The main game loop
+function mainLoop(timecalled) {
+    if (timecalled < lastTimeCalled + (1000 / maxFPS)) {
+        frameID = requestAnimationFrame(mainLoop);
+        return;
     }
+    delta = timecalled - lastTimeCalled; // get the delta time since last frame
+    lastTimeCalled = timecalled;
 
-    // Updates the state
-    function update(delta) {
-        
-        if(testVar === 20) {
-            testVar++;
-            testSig = 'pos';
-        }else if(testVar === 50) {
-            testVar--;
-            testSig = 'neg';
-        }else if(testSig === 'pos') {
-            testVar++;
-        }else if(testSig === 'neg') {
-            testVar--;
-        }
+    update(delta);
+    drawCanvas();
+    frameID = requestAnimationFrame(mainLoop);
+}
+
+// Updates the state
+function update(delta) {
+    if(testVar <= 20) {
+        testVar += testStep*delta;
+        testSig = 'pos';
+    }else if(testVar >= 50) {
+        testVar -= testStep*delta;
+        testSig = 'neg';
+    }else if(testSig === 'pos') {
+        testVar += testStep*delta;
+    }else if(testSig === 'neg') {
+        testVar -= testStep*delta;
     }
+}
 
-    // Draws the canvas according to state
-    function drawCanvas() {
-        ctx.clearRect((cnv.width/2)*-1, (cnv.height/2)*-1, cnv.width, cnv.height);
-        ctx.rotate(Math.PI*2/600);
-        ctx.strokeRect(-75, 0 - testVar/2, 150, testVar);
+// Draws the canvas according to state
+function drawCanvas() {
+    ctx.clearRect((cnv.width/2)*-1, (cnv.height/2)*-1, cnv.width, cnv.height);
+    ctx.rotate(Math.PI*2/600);
+    ctx.strokeRect(-75, 0 - testVar/2, 150, testVar);
+}
+
+function toggleRun() {
+    isRunning = isRunning ? false : true;
+    if(isRunning) {
+        startRun();
     }
+    else {
+        stopRun();
+    }
+}
 
-    // RUN! Starts the 'game'...
-    requestAnimationFrame(mainLoop);
+function stopRun() {
+    cancelAnimationFrame(frameID);
+}
 
-})();
+function startRun() {
+    frameID = requestAnimationFrame(function(timestamp) {
+        lastTimeCalled = timestamp;
+        frameID = requestAnimationFrame(mainLoop);
+    });
+}
+
+// RUN! Starts the 'game'...
+requestAnimationFrame(mainLoop);
